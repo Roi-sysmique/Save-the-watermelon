f3da50b871ee5dccb6da811839f0cae5a823f865
Roi-sysmique <hamzaelhechmi@gmail.com>
2024-10-06 13:46:57 +0100

    'add-projectile'

--- main.py
+++ main.py
@@ -9,6 +9,7 @@ SCREEN = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 pygame.display.set_caption("pygame")
 clock = pygame.time.Clock()
 BACKGROUND = pygame.surface.Surface((SCREEN_WIDTH, SCREEN_HEIGHT))
+BACKGROUND.fill('blue')
 title = pygame.transform.rotozoom(pygame.image.load('Assets/title.png'), 0, 0.5)
 title_rect = title.get_rect(bottomright=(SCREEN_WIDTH-10, SCREEN_HEIGHT-10))
 title_y_position = 0
@@ -35,24 +36,56 @@ class WaterMellon(pygame.sprite.Sprite):
         self.rect.center = (SCREEN_WIDTH/2, SCREEN_HEIGHT*2/3 + 100)
 
 
+class WatermelonSeed(pygame.sprite.Sprite):
+
+    def __init__(self, pos_x, pos_y, angle):
+        pygame.sprite.Sprite.__init__(self)
+        self.angle = angle
+        self.original_image = pygame.image.load('Assets/watermelon-seed.png')
+        self.image = pygame.transform.rotozoom(self.original_image, self.angle, 0.5)
+        self.rect = self.image.get_rect()
+        self.x = pos_x
+        self.y = pos_y
+        self.rect.center = (self.x, self.y)
+        self.velocity = 6
+        self.theta = self.angle + 90
+        self.x_velocity = - self.velocity * math.cos(math.radians(self.theta))
+        self.y_velocity = self.velocity * math.sin(math.radians(self.theta))
+
+    def update(self):
+        self.image = pygame.transform.rotozoom(self.original_image, self.angle, 0.5)
+        self.y -= self.y_velocity
+        self.x -= self.x_velocity
+        self.rect.center = (self.x, self.y)
+        if self.rect.centery > 600 or self.rect.centerx > 700:
+            self.kill()
+
+
 player = WaterMellon()
+Projectiles = pygame.sprite.Group()
 
 
 def title_animation():
     global title_y_position
     title_y_position += 0.04
-    title_rect.bottomright = (SCREEN_WIDTH-10 , (SCREEN_HEIGHT-10) + 10*math.sin(title_y_position))
+    title_rect.bottomright = (SCREEN_WIDTH-10, (SCREEN_HEIGHT-10) + 10*math.sin(title_y_position))
 
 
 while True:
+    Projectiles.update()
     keys = pygame.key.get_pressed()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             pygame.quit()
             quit()
+        if event.type == pygame.KEYDOWN:
+            if event.key == pygame.K_SPACE:
+                projectile = WatermelonSeed(player.rect.center[0], player.rect.center[1], player.angle)
+                Projectiles.add(projectile)
 
     player.update(keys)
     SCREEN.blit(BACKGROUND, (0, 0))
+    Projectiles.draw(SCREEN)
     SCREEN.blit(player.image, player.rect)
     SCREEN.blit(title, title_rect)
     pygame.display.update()
